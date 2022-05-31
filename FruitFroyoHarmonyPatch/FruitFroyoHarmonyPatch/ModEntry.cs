6f83a204ed6715f50975021bc5f6a41b48d4ed7a
failsplat <68579200+failsplat@users.noreply.github.com>
2022-05-31 18:55:08 -0400

    Harmony Patch Groundwork

---
+++ FruitFroyoHarmonyPatch/FruitFroyoHarmonyPatch/ModEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using HarmonyLib;
+
+namespace FruitFroyo
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            this.ApplyPatches(new Harmony(this.ModManifest.UniqueID));
+        }
+
+        private void ApplyPatches(Harmony harmony)
+        {
+
+        }
+    }
+}
